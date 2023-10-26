324fe94736ab8d62f1e0faa4ca2dd84e4d7259b8
juliano rodrigues <dev@hdeletro.com.br>
2023-10-26 14:09:25 -0300

    Refactor definições wifi

--- main/wifi/wifi.c
+++ main/wifi/wifi.c
@@ -13,33 +13,33 @@
 
 #include "wifi.h"
 
-#define WIFI_TENTATIVAS_RECONEXAO 5
+#define WIFI_MAX_TENTATIVAS_RECONEXAO 5
 
-#define WIFI_CONNECTED_BIT  BIT0
-#define WIFI_FAIL_BIT       BIT1
+#define WIFI_BIT_CONECTADO  BIT0
+#define WIFI_BIT_FALHA      BIT1
 
 #define TAG "WIFI"
 
-static EventGroupHandle_t s_wifi_event_group;
-static uint16_t s_retry_num = 0;
+static EventGroupHandle_t wifi_grupo_eventos;
+static uint16_t cont_tentativas = 0;
 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+static void event_handler(void *arg, esp_event_base_t evento_base, int32_t evento_id, void *evento_dados) {
+    if ((evento_base == WIFI_EVENT) && (evento_id == WIFI_EVENT_STA_START)) {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < WIFI_TENTATIVAS_RECONEXAO) {
+    } else if ((evento_base == WIFI_EVENT) && (evento_id == WIFI_EVENT_STA_DISCONNECTED)) {
+        if (cont_tentativas < WIFI_MAX_TENTATIVAS_RECONEXAO) {
             esp_wifi_connect();
-            s_retry_num++;
+            cont_tentativas++;
             ESP_LOGI(TAG, "Tentando reconectar a rede WiFi...");
         } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            xEventGroupSetBits(wifi_grupo_eventos, WIFI_BIT_FALHA);
         }
         ESP_LOGI(TAG, "Falha ao conectar na rede WiFi.");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
-        ESP_LOGI(TAG, "Endereco IP recebido:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    } else if ((evento_base == IP_EVENT) && (evento_id == IP_EVENT_STA_GOT_IP)) {
+        ip_event_got_ip_t *evento = (ip_event_got_ip_t *) evento_dados;
+        ESP_LOGI(TAG, "Endereco IP recebido:" IPSTR, IP2STR(&evento->ip_info.ip));
+        cont_tentativas = 0;
+        xEventGroupSetBits(wifi_grupo_eventos, WIFI_BIT_CONECTADO);
         xSemaphoreGive(semaph_con_wifi);
         xSemaphoreGive(semaph_con_sntp);
     }
@@ -48,20 +48,18 @@ static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_
 void wifi_init(const char *wifi_ssid, const char *wifi_pass) {
     semaph_con_wifi = xSemaphoreCreateBinary();
     semaph_con_sntp = xSemaphoreCreateBinary();
-    s_wifi_event_group = xEventGroupCreate();
+    wifi_grupo_eventos = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    
+
     esp_netif_create_default_wifi_sta();
 
-    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
+    wifi_init_config_t init_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&init_config));
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-
-    
 
     wifi_config_t config = {
         .sta = {
@@ -78,12 +76,12 @@ void wifi_init(const char *wifi_ssid, const char *wifi_pass) {
 
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+    EventBits_t bits = xEventGroupWaitBits(wifi_grupo_eventos, WIFI_BIT_CONECTADO | WIFI_BIT_FALHA, pdFALSE, pdFALSE, portMAX_DELAY);
 
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "Conectado na rede WiFi: %s, senha: %s", WIFI_SSID, WIFI_PASS);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Falha ao conectar na rede WiFi: %s, senha: %s", WIFI_SSID, WIFI_PASS);
+    if (bits & WIFI_BIT_CONECTADO) {
+        ESP_LOGI(TAG, "Conectado na rede WiFi: %s, senha: %s", config.sta.ssid, config.sta.password);
+    } else if (bits & WIFI_BIT_FALHA) {
+        ESP_LOGI(TAG, "Falha ao conectar na rede WiFi: %s, senha: %s", config.sta.ssid, config.sta.password);
     } else {
         ESP_LOGE(TAG, "Evento inesperado.");
     }
